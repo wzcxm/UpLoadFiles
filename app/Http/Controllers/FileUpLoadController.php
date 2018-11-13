f685ad6a403fab955317c3fba2907a4ebc330b2d
斯蒂芬程 <dark_zc@aliyun.com>
2018-11-13 16:12:28 +0800

    201811131612

--- app/Http/Controllers/FileUpLoadController.php
+++ app/Http/Controllers/FileUpLoadController.php
@@ -19,33 +19,43 @@ class FileUpLoadController extends Controller
      * @return array
      */
     public function FileUpLoad(Request $request){
-        $uid = $request->input('uid','');
-        $key = $request->input('key','');
-        $user =  DB::table('pro_mall_users')->where([['uid',$uid],['file_key',$key]])->count();
-        //验证用户是uid或key是否错误
-        if(empty($user)){
-            return ['success'=>'','error'=>'uid 或 key 错误'];
-        }
-        $files = $request->allFiles();
-        $returnPath = [];
-        $error = [];
-        foreach ($files as $file){
-            if($file -> isValid()){
-                //检验一下上传的文件是否有效
-                $clientName = $file -> getClientOriginalName(); //获取文件名称
-                $ext = $file -> getClientOriginalExtension();  //上传文件的后缀
-                $size = $file->getSize();
-                if($size >= 2*1024*1024){
-                    $error[] =  '文件：['.$clientName.']上传失败，文件大于2M';
-                }else{
-                    $dir = $uid.'/'.date('Y-m-d').'/';
-                    $newName = md5($clientName.rand(0,999)).'.'.$ext;
-                    $file -> move('files/'.$dir,$newName);
-                    $returnPath[] = env('FILE_HTTP_URL').$dir.$newName;
+        $retJson = new ReturnData();
+        try{
+            $uid = $request->input('uid','');
+            $key = $request->input('key','');
+            $user =  DB::table('pro_mall_users')->where([['uid',$uid],['file_key',$key]])->count();
+            //验证用户是uid或key是否错误
+            if(empty($user)){
+                $retJson->code = 400;
+                $retJson->message = 'uid或key是否错误';
+                return $retJson->toJson();
+            }
+            $files = $request->allFiles();
+            $returnPath = [];
+            $error = [];
+            foreach ($files as $file){
+                if($file -> isValid()){
+                    //检验一下上传的文件是否有效
+                    $clientName = $file -> getClientOriginalName(); //获取文件名称
+                    $ext = $file -> getClientOriginalExtension();  //上传文件的后缀
+                    $size = $file->getSize();
+                    if($size >= 2*1024*1024){
+                        $error[] =  '文件：['.$clientName.']上传失败，文件大于2M';
+                    }else{
+                        $dir = $uid.'/'.date('Y-m-d').'/';
+                        $newName = md5($clientName.rand(0,999)).'.'.$ext;
+                        $file -> move('files/'.$dir,$newName);
+                        $returnPath[] = env('FILE_HTTP_URL').$dir.$newName;
+                    }
                 }
             }
+            $retJson->data = ['success'=>$returnPath,'error'=>$error];
+            return $retJson->toJson();
+        }catch (\Exception $e){
+            $retJson->code = 300;
+            $retJson->message = $e->getMessage();
+            return $retJson->toJson();
         }
-        return ['success'=>$returnPath,'error'=>$error];
     }
 
     /**
@@ -54,17 +64,22 @@ class FileUpLoadController extends Controller
      * @return array
      */
     public function  DeleteFile(Request $request){
+        $retJson = new ReturnData();
         try{
             $uid = $request->input('uid','');
             $key = $request->input('key','');
             $urls = $request->input('urls','');
+            if(empty($urls)){
+                $retJson->code = 400;
+                $retJson->message = 'urls不能为空';
+                return $retJson->toJson();
+            }
             $user =  DB::table('pro_mall_users')->where([['uid',$uid],['file_key',$key]])->count();
             //验证用户是uid或key是否错误
             if(empty($user)){
-                return ['error'=>'uid 或 key 错误'];
-            }
-            if(empty($urls)){
-                return ['error'=>'文件地址不能为空'];
+                $retJson->code = 400;
+                $retJson->message = 'uid或key是否错误';
+                return $retJson->toJson();
             }
             $url_arr = explode('|',$urls);
             $error = [];
@@ -76,16 +91,32 @@ class FileUpLoadController extends Controller
                     $error[] = $url.' [删除失败]';
                 }
             }
-            if(empty($error)){
-                return ['success'=>'ok'];
-            }else{
-                return ['error'=>$error];
+            if(!empty($error)){
+                $retJson->data = ['error_info'=>$error];
             }
-
+            return $retJson->toJson();
         }catch (\Exception $e){
-            return ['error'=>$e->getMessage()];
+            $retJson->code = 300;
+            $retJson->message = $e->getMessage();
+            return $retJson->toJson();
         }
 
     }
 
+}
+
+
+class ReturnData
+{
+    public $code = 0;
+    public $message='success';
+    public $data;
+    public  function toJson(){
+        $ret_data = [
+            'code' => $this->code,
+            'message' => $this->message,
+            'data' => $this->data
+        ];
+        return urldecode(json_encode($ret_data));
+    }
 }
